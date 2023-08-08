58de38bf5726462f906f3622c1e083937c14bda5
YingjiangTang <75183035+YingjiangTang@users.noreply.github.com>
2023-08-07 23:24:06 -0400

    Update website4chenzhi.py

--- website4chenzhi.py
+++ website4chenzhi.py
@@ -1,8 +1,6 @@
 # Import the necessary packages
 import streamlit as st
 import pandas as pd
-import joblib
-import pickle
 import numpy as np
 import math
 import os
@@ -14,11 +12,5 @@ st.set_page_config(page_title="MaGeNiuBi", layout="wide")
 # Set up the header
 with st.container():
     st.header('Resistance of Concrete-Filled Steel Tubular (CFST) Columns Predicted by Machine Learning Models')
-
-
-# Load models and scalers
-# Circular columns
-CC_GBR=joblib.load(os.path.join(ROOT_DIR,'CFST_Circ_Columns_GBR.joblib'))
-CC_GBR_sc=pickle.load(open(os.path.join(ROOT_DIR,'CFST_Circ_Columns_GBR.pkl'),'rb'))
 
 
